f06bad2c09d8c491b303cad3b8b064bc00b20bae
Duapar Dev <duapar@dev.local>
2026-07-17 01:39:22 +0200

    feat: classe principale DEvent - câblage

---
+++ src/main/java/com/devent/DEvent.java
@@ -0,0 +1,42 @@
+package com.devent;
+
+import com.devent.commands.EventCommand;
+import com.devent.integration.DAPIHook;
+import com.devent.manager.EventManager;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DEvent extends JavaPlugin {
+
+    private EventManager eventManager;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        eventManager = new EventManager(this);
+        eventManager.loadEvents(getConfig(), getLogger());
+        eventManager.scheduleAll();
+
+        EventCommand eventCommand = new EventCommand(eventManager);
+        PluginCommand event = getCommand("event");
+        if (event != null) {
+            event.setExecutor(eventCommand);
+        }
+
+        if (getServer().getPluginManager().isPluginEnabled("DAPI")) {
+            DAPIHook.registerPlugin(this);
+        } else {
+            getLogger().info("DAPI non détecté : DEvent fonctionne en mode autonome (pas de mise en vente automatique).");
+        }
+
+        getLogger().info("DEvent activé (" + eventManager.listEvents().size() + " événement(s) planifié(s)).");
+    }
+
+    @Override
+    public void onDisable() {
+        if (eventManager != null) {
+            eventManager.cancelAll();
+        }
+    }
+}
